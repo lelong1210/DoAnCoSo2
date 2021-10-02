79a4ba40412d516833b9e7573ea2be801c02ffbc
LONG LÊ QUANG <you@example.com>
2021-10-02 15:30:59 +0700

    add all file

--- mvc/controllers/home.php
+++ mvc/controllers/home.php
@@ -3,6 +3,7 @@ class home extends controller{
     function show($hello){
         $model = $this->call_model("homeModel");
         $this->call_view("homeView",["homeModel"=>$model]);
+        // 
     }
 }
 ?>
