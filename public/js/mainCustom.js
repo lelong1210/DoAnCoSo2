793bbf11d7718195a3aab41d471b8701b46d2a65
Your Name <you@example.com>
2021-10-17 23:03:55 +0700

    add all file

--- public/js/mainCustom.js
+++ public/js/mainCustom.js
@@ -1,30 +1,66 @@
 /*****************CUSTOM************************/
-
+var result = true;
 $(document).ready(function () {
-    let ErrDk = true;
-    let arr = ["tendangnhap_DK", "matkhau_DK", "rematkhau_DK", "email_DK"];
-    $("#dangky").click(function (e) {
-        alert(ErrDk);
-        var tendangnhap = $("#" + arr[0]).val();
-        var matkhau = $("#" + arr[1]).val();
-        var rematkhau = $("#" + arr[2]).val();
-        var email = $("#" + arr[3]).val();
-        if (tendangnhap == "" || matkhau == "" || matkhau == "" || email == "") {
-            alert("các ô không được để trồng");
-        } else {
-            if (ErrDk) {
-                $.post("./ajax/dangky", { tendangnhap: tendangnhap, matkhau: matkhau, email: email }, function (data) {
-                    alert(data);
-                });
-            } else {
-                alert("loi tren man hinh");
-            }
-        }
-    });
     $("#dangnhap").click(function (e) {
         var tendangnhap = $("#tendangnhap_DN").val();
         var matkhau = $("#matkhau_DN").val();
-
+        dangNhap(tendangnhap, matkhau);
+    });
+    $("#dangky").click(function () {
+        let arrDk = ["tendangnhap_DK","matkhau_DK","rematkhau_DK","email_DK"];
+        
+    });
+    $("input").keyup(function (e) { 
+        let arrDk = ["tendangnhap_DK","matkhau_DK","rematkhau_DK","email_DK"];
+        if($(this).attr('id') == arrDk[0]){
+            if(checkAcount($(this).val())){
+                spanErr($(this).attr('id'),false,"Tồn Tại...");
+            }else{
+                spanErr($(this).attr('id'),true,"");
+            }
+        }
+        if($(this).attr('id') == arrDk[2]){
+            var pass1 = $("#"+arrDk[1]).val();
+            var pass2 = $(this).val();
+            if(comparePassword(pass1,pass2)){
+                spanErr($(this).attr('id'),true,"");
+            }else{
+                spanErr($(this).attr('id'),false,"Mật Khẩu Không Khớp...");
+            }  
+        }
+        if($(this).attr('id') == arrDk[3]){
+            var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+            var email = $(this).val();
+            if(pattern.test(email)){
+                
+            }else{
+                
+            }    
+        }
+    });
+    // function support 
+    function checkAcount(tendangnhap){
+        var php_data;
+        $.ajax({
+            type: "post",
+            url: "./ajax/CheckAcount",
+            async: false, 
+            data: {tendangnhap:tendangnhap},
+            // dataType: "dataType",
+            success: function (response) {
+                php_data = response;
+            }
+        });
+        return php_data;
+    }
+    function comparePassword(pass1,pass2) {
+        if (pass1 == pass2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function dangNhap(tendangnhap, matkhau) {
         $.post("./ajax/dangnhap", { tendangnhap: tendangnhap, matkhau: matkhau }, function (data) {
             if (data) {
                 location.replace("./taikhoan");
@@ -32,60 +68,22 @@ $(document).ready(function () {
                 alert("DANG NHAP THAT BAI");
             }
         });
-    });
-    $("input").keyup(function (e) {
-        var id = $(this).attr('id');
-        if (id == arr[0]) {
-            var tendangnhap = $(this).val();
-            $.post("./ajax/checkAcount", { tendangnhap: tendangnhap }, function (data) {
-                if (data) {
-                    ErrDk = false;
-                    $("#" + "sp" + "tendangnhap_DK").css({ "color": "#fa0000 !important" });
-                    $("#" + "sp" + "tendangnhap_DK").html(ErrDk);
-                    
-                } else {
-                    ErrDk = true;
-                    $("#" + "sp" + "tendangnhap_DK").html(ErrDk);
-                }
-            });
-        }
-        if (id == arr[2]) {
-            var pass1 = $("#" + arr[1]).val();
-            var pass2 = $(this).val();
-            // alert(pass2+"==="+pass1);
-            if (comparePassword(pass1, pass2)) {
-                $("#" + "sp" + arr[2]).html("");
-                ErrDk = true;
-            } else {
-                $("#" + "sp" + arr[2]).css({ "color": "#fa0000 !important" });
-                $("#" + "sp" + arr[2]).html("mật khẩu không trùng khớp");
-                ErrDk = false;
-            }
-        }
-    });
-});
-function comparePassword($pass1, $pass2) {
-    if ($pass1 == $pass2) {
-        return true;
-    } else {
-        return false;
     }
-}
-function spanErr(idName) {
-    // $("#sp"+idName).html($("#"+idName).val());
-    // $("#sp"+idName).css({"color":"red","font-size":"small"});
-    // $("#sp"+idName).addClass("fas fa-times");
-
-}
-function checkAcount(tendangnhap) {
-    $.post("./ajax/checkAcount", { tendangnhap: tendangnhap }, function (data) {
-        if (data) {
-            $("#" + "sp" + "tendangnhap_DK").css({ "color": "#fa0000 !important" });
-            $("#" + "sp" + "tendangnhap_DK").html("tài khoản đã tồn tại");
-            ErrDk = false;
-        } else {
-            $("#" + "sp" + "tendangnhap_DK").html("");
-            ErrDk = true;
+    function spanErr(idName,option,mess) {
+        if(option){
+            $("#sp"+idName).html("");
+            $("#sp"+idName).css({"color":"red","font-size":"small"});
+        }else{
+            $("#sp"+idName).html(mess);
+            $("#sp"+idName).css({"color":"red","font-size":"small"});
         }
-    });
-}
+        // $("#sp"+idName).addClass("fas fa-times");
+    
+    }
+
+});
+
+
+
+
+
