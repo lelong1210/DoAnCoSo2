033a8b0bbcaf2bdfe287566433fbc03ef3ab9f3f
Your Name <you@example.com>
2021-10-18 20:47:13 +0700

    add all file

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     session_start();
     require_once "./mvc/bridge.php";
     new app();//
+    // call xampp ==> docker run --name myXampp -p 41061:22 -p 80:80 -d -v /media/lql/E/Code/Code_Php/DoAnCoSo2/:/www tomsik68/xampp
+    // start ==> docker start be70629c0331 
+
 ?>
