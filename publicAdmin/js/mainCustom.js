85bd53e26332bc3390e79d18e4265f2d0106fada
Your Name <you@example.com>
2021-10-18 21:58:03 +0700

    add all file

--- publicAdmin/js/mainCustom.js
+++ publicAdmin/js/mainCustom.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $("#calendarCustom").click(function (e) { 
-       var pageCalendar  =  "./pageAdmin/calendarpage"; 
-       $("#content-page").load(pageCalendar);
+
     });
 });
