45922bda2a9a959857ea0b2640ff0e3bbc001b40
Your Name <you@example.com>
2021-11-13 00:34:57 +0700

    add all file

--- mvc/views/homeView.php
+++ mvc/views/homeView.php
@@ -2,51 +2,50 @@
 <html lang="zxx">
 
 <head>
-    <?php require_once "block/head.php"; 
-        if(isset($_GET['code'])){
-            // require_once './mvc/vendor/autoload.php';
-require_once "./mvc/google/vendor/autoload.php";
-// init configuration
-$clientID = '892983593171-fue6cco6othcpsp4jdrrde3ctijtur1i.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://localhost/www/';
+    <?php require_once "block/head.php";
+    if (isset($_GET['code'])) {
+        // require_once './mvc/vendor/autoload.php';
+        require_once "./mvc/google/vendor/autoload.php";
+        // init configuration
+        $clientID = '892983593171-fue6cco6othcpsp4jdrrde3ctijtur1i.apps.googleusercontent.com';
+        $clientSecret = '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';
+        $redirectUri = 'http://localhost/www/';
 
-// create Client Request to access Google API
-$client = new Google_Client();
-$client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectUri);
-$client->addScope("email");
-$client->addScope("profile");
+        // create Client Request to access Google API
+        $client = new Google_Client();
+        $client->setClientId($clientID);
+        $client->setClientSecret($clientSecret);
+        $client->setRedirectUri($redirectUri);
+        $client->addScope("email");
+        $client->addScope("profile");
 
-            $_SESSION['code'] = $_GET['code'];
-            if (isset($_SESSION['code'])) {
-                $token = $client->fetchAccessTokenWithAuthCode($_SESSION['code']);
-                $client->setAccessToken($token['access_token']);
-              
-                // get profile info
-                $google_oauth = new Google_Service_Oauth2($client);
-                $google_account_info = $google_oauth->userinfo->get();
-                $email =  $google_account_info->email;
-                $name =  $google_account_info->name;
-                //
-                $tendangnhap = $email;
-                $ngaythamgia = date("Y-m-d");
-                $matkhau = "";
-                if ($data["taikhoanModel"]->checkAcount($tendangnhap)) {
-                  $data["taikhoanModel"]->dangnhap($tendangnhap, $matkhau);
-                } else {
-                  if ($data["taikhoanModel"]->dangky($tendangnhap, "", $email, 0, $ngaythamgia)) {
+        $_SESSION['code'] = $_GET['code'];
+        if (isset($_SESSION['code'])) {
+            $token = $client->fetchAccessTokenWithAuthCode($_SESSION['code']);
+            $client->setAccessToken($token['access_token']);
+
+            // get profile info
+            $google_oauth = new Google_Service_Oauth2($client);
+            $google_account_info = $google_oauth->userinfo->get();
+            $email =  $google_account_info->email;
+            $name =  $google_account_info->name;
+            //
+            $tendangnhap = $email;
+            $ngaythamgia = date("Y-m-d");
+            $matkhau = "";
+            if ($data["taikhoanModel"]->checkAcount($tendangnhap)) {
+                $data["taikhoanModel"]->dangnhap($tendangnhap, $matkhau);
+            } else {
+                if ($data["taikhoanModel"]->dangky($tendangnhap, "", $email, 0, $ngaythamgia)) {
                     $data["taikhoanModel"]->dangnhap($tendangnhap, $matkhau);
-                  }
                 }
-                unset($_SESSION['code']);
-                header('Location: /www/dndk');
-                // echo $google_account_info;
-                // now you can use this profile info to create account in your website and make user logged in.
-              } 
-
+            }
+            unset($_SESSION['code']);
+            header('Location: /www/dndk');
+            // echo $google_account_info;
+            // now you can use this profile info to create account in your website and make user logged in.
         }
+    }
     ?>
     <title>Trang Chủ</title>
 </head>
