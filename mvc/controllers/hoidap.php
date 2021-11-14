a9cf394212d423ab27463fdfa0b3c7f807567a5a
Your Name <you@example.com>
2021-11-14 23:41:20 +0700

    add all file

--- mvc/controllers/hoidap.php
+++ mvc/controllers/hoidap.php
@@ -1,5 +1,8 @@
 <?php
-class chat extends controller{
+class hoidap extends controller{
+    function __construct(){
+        $this->check_user_quyen(0);    
+    }
     function show(){
         $model = $this->call_model("chatModel");
         $this->call_view("chatView",[
