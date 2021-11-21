c92ca67df622869a024d8613bff4e5c3769806f9
Your Name <you@example.com>
2021-11-21 23:47:55 +0700

    add all file

--- mvc/views/block/header.php
+++ mvc/views/block/header.php
@@ -135,7 +135,7 @@
                                 <!-- <li class="dropdown"><a href="#">Tin Tức</a></li> -->
                                 <li class="dropdown"><a href="/www/hoidap">Hỏi Đáp</a></li>
                                 <li><a href="/www/lienhe">Liên Hệ</a></li>
-                                <li class="dropdown" id="drakMode"><a><i class="fas fa-exchange-alt"></i></a></li>
+                                <!-- <li class="dropdown" id="drakMode"><a><i class="fas fa-exchange-alt"></i></a></li> -->
                             </ul>
                         </div>
                     </div>
